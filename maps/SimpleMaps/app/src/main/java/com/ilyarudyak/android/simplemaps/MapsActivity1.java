05958c4abb5114914b326639c91b824acd7c4e03
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-02-09 00:16:15 +0300

    add Activity 2

--- maps/SimpleMaps/app/src/main/java/com/ilyarudyak/android/simplemaps/MapsActivity1.java
+++ maps/SimpleMaps/app/src/main/java/com/ilyarudyak/android/simplemaps/MapsActivity1.java
@@ -3,11 +3,11 @@ package com.ilyarudyak.android.simplemaps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MapsActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
     }
 }
