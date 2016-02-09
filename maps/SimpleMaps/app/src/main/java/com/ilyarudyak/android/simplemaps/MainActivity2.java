3b6ac77126476fb3503a3dbabc6a6d0895b6b09a
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-02-09 15:23:08 +0300

    add button listeners with butterknife

--- maps/SimpleMaps/app/src/main/java/com/ilyarudyak/android/simplemaps/MainActivity2.java
+++ maps/SimpleMaps/app/src/main/java/com/ilyarudyak/android/simplemaps/MainActivity2.java
@@ -2,6 +2,7 @@ package com.ilyarudyak.android.simplemaps;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +10,9 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity2 extends AppCompatActivity
     implements OnMapReadyCallback {
@@ -21,8 +25,29 @@ public class MainActivity2 extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        ButterKnife.bind(this);
+
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    @OnClick({ R.id.btn_map, R.id.btn_satellite, R.id.btn_hybrid })
+    public void onBtnClicked(View v) {
+        if (mMapReady) {
+            switch (v.getId()) {
+                case R.id.btn_map:
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    break;
+                case R.id.btn_satellite:
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                    break;
+                case R.id.btn_hybrid:
+                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    break;
+                default:
+                    throw new IllegalArgumentException("illegal id");
+            }
+        }
     }
 
     @Override
