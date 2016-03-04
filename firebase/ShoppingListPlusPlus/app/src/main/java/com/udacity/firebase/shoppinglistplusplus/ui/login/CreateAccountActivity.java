ca79bbf5bec9a68c528108aba94524fa76b706ab
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-03-04 15:57:33 +0300

    add create user

--- firebase/ShoppingListPlusPlus/app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/login/CreateAccountActivity.java
+++ firebase/ShoppingListPlusPlus/app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/login/CreateAccountActivity.java
@@ -3,47 +3,39 @@ package com.udacity.firebase.shoppinglistplusplus.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.udacity.firebase.shoppinglistplusplus.R;
 import com.udacity.firebase.shoppinglistplusplus.ui.BaseActivity;
+import com.udacity.firebase.shoppinglistplusplus.utils.Constants;
+
+import java.util.Map;
 
 public class CreateAccountActivity extends BaseActivity {
 
     private static final String LOG_TAG = CreateAccountActivity.class.getSimpleName();
     private ProgressDialog mAuthProgressDialog;
     private EditText mEditTextUsernameCreate, mEditTextEmailCreate, mEditTextPasswordCreate;
-
+    private Firebase mFirebaseRef;
+    private String mUserName, mUserEmail, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        /**
-         * Link layout elements from XML and setup the progress dialog
-         */
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+
         initializeScreen();
     }
 
-    /**
-     * Override onCreateOptionsMenu to inflate nothing
-     *
-     * @param menu The menu with which nothing will happen
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
-
-
-    /**
-     * Link layout elements from XML and setup the progress dialog
-     */
+    // helper methods
     public void initializeScreen() {
         mEditTextUsernameCreate = (EditText) findViewById(R.id.edit_text_username_create);
         mEditTextEmailCreate = (EditText) findViewById(R.id.edit_text_email_create);
@@ -57,46 +49,85 @@ public class CreateAccountActivity extends BaseActivity {
         mAuthProgressDialog.setMessage(getResources().getString(R.string.progress_dialog_creating_user_with_firebase));
         mAuthProgressDialog.setCancelable(false);
     }
+    private void createUserInFirebaseHelper(final String encodedEmail) {
+    }
+    private boolean isEmailValid(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEditTextEmailCreate.setError(String.format(getString(R.string.error_invalid_email_not_valid),
+                    email));
+            return false;
+        }
+        return isGoodEmail;
+    }
+    private boolean isUserNameValid(String userName) {
+        if (userName.equals("")) {
+            mEditTextUsernameCreate.setError(getResources().getString(R.string.error_cannot_be_empty));
+            return false;
+        }
+        return true;
+    }
+    private boolean isPasswordValid(String password) {
+        if (password.length() < 6) {
+            mEditTextPasswordCreate.setError(getResources().getString(R.string.error_invalid_password_not_valid));
+            return false;
+        }
+        return true;
+    }
+    private void showErrorToast(String message) {
+        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
+    }
 
-    /**
-     * Open LoginActivity when user taps on "Sign in" textView
-     */
+    // button listeners
     public void onSignInPressed(View view) {
         Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
-
-    /**
-     * Create new account using Firebase email/password provider
-     */
     public void onCreateAccountPressed(View view) {
+        mUserName = mEditTextUsernameCreate.getText().toString();
+        mUserEmail = mEditTextEmailCreate.getText().toString().toLowerCase();
+        mPassword = mEditTextPasswordCreate.getText().toString();
 
-    }
+        if (isUserNameValid(mUserName) && isEmailValid(mUserEmail) && isPasswordValid(mPassword)) {
+            mFirebaseRef.createUser(mUserEmail, mPassword, new Firebase.ValueResultHandler<Map<String, Object>>() {
 
-    /**
-     * Creates a new user in Firebase from the Java POJO
-     */
-    private void createUserInFirebaseHelper(final String encodedEmail) {
-    }
+                @Override
+                public void onSuccess(Map<String, Object> stringObjectMap) {
+                    Log.d(LOG_TAG, getString(R.string.log_message_auth_successful));
+                }
 
-    private boolean isEmailValid(String email) {
-        return true;
-    }
-
-    private boolean isUserNameValid(String userName) {
-        return true;
-    }
-
-    private boolean isPasswordValid(String password) {
-        return true;
-    }
-
-    /**
-     * Show error toast to users
-     */
-    private void showErrorToast(String message) {
-        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    Log.d(LOG_TAG, getString(R.string.log_error_occurred) + firebaseError);
+                    if (firebaseError.getCode() == FirebaseError.EMAIL_TAKEN) {
+                        mEditTextEmailCreate.setError(getString(R.string.error_email_taken));
+                    } else {
+                        showErrorToast(firebaseError.getMessage());
+                    }
+                }
+            });
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
